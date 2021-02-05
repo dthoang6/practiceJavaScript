2c4927476145e573a28084bb5aaf86ed1abcd2d7
Dat Hoang <dthoang6@gmail.com>
2021-02-05 12:30:25 -0800

    using if and else if statement to make decisions based on logical conditionl

---
+++ golf_code.js
@@ -0,0 +1,29 @@
+/*using if statement, else if statement to make conditional logic decision*/
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  if (strokes == 1) {
+    return "Hole-in-one!"
+  }
+  else if ( strokes <= par -2) {
+    return "Eagle";
+  }
+  else if ( strokes <= par -1) {
+    return "Birdie";
+  }
+  else if (strokes <= par) {
+    return "Par";
+  }
+  else if (strokes <= par + 1) {
+    return "Bogey";
+  }
+  else if (strokes <= par + 2) {
+    return "Double Bogey";
+  }
+  else  if (strokes >= par + 3){
+    return "Go Home!";
+  }
+}
+
+console.log(golfScore(5, 4));
